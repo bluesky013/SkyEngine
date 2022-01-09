ae3ee7afa85c1bd4e2dea2000eb21ac399732b1c
Lizhen <bluesky013@126.com>
2022-01-10 01:55:02 +0800

    [feature] : Added renderpass to cache

--- vulkan/include/vulkan/GraphicsPipeline.h
+++ vulkan/include/vulkan/GraphicsPipeline.h
@@ -6,6 +6,7 @@
 
 #include "vulkan/DevObject.h"
 #include "vulkan/vulkan.h"
+#include <vector>
 
 namespace sky::drv {
 
@@ -15,7 +16,60 @@ namespace sky::drv {
     public:
         ~GraphicsPipeline() = default;
 
+        struct InputAssembly {
+            VkPrimitiveTopology topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+            VkBool32            primitiveRestartEnable = VK_FALSE;
+        };
+
+        struct Raster {
+            VkBool32        depthClampEnable        = VK_FALSE;
+            VkBool32        rasterizerDiscardEnable = VK_FALSE;
+            VkPolygonMode   polygonMode             = VK_POLYGON_MODE_FILL;
+            VkCullModeFlags cullMode                = VK_CULL_MODE_NONE;
+            VkFrontFace     frontFace               = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+            VkBool32        depthBiasEnable         = VK_FALSE;
+            float           depthBiasConstantFactor = 0.f;
+            float           depthBiasClamp          = 0.f;
+            float           depthBiasSlopeFactor    = 0.f;
+            float           lineWidth               = 1.f;
+        };
+
+        struct MultiSample {
+            VkSampleCountFlagBits rasterizationSamples  = VK_SAMPLE_COUNT_1_BIT;
+            VkBool32              sampleShadingEnable   = VK_FALSE;
+            float                 minSampleShading      = 0.f;
+            VkBool32              alphaToCoverageEnable = VK_FALSE;
+            VkBool32              alphaToOneEnable      = VK_FALSE;
+        };
+
+        struct DepthStencilState {
+            VkBool32         depthTestEnable = false;
+            VkBool32         depthWriteEnable = false;
+            VkCompareOp      depthCompareOp = VK_COMPARE_OP_NEVER;
+            VkBool32         depthBoundsTestEnable = VK_FALSE;
+            VkBool32         stencilTestEnable = VK_FALSE;
+            VkStencilOpState front = {};
+            VkStencilOpState back  = {};
+            float            minDepthBounds = 0.f;
+            float            maxDepthBounds = 1.f;
+        };
+
+        struct ColorBlend {
+            std::vector<VkPipelineColorBlendAttachmentState> attachments;
+        };
+
+        struct PipelineState {
+            InputAssembly inputAssembly;
+            Raster raster;
+            MultiSample multiSample;
+            ColorBlend blends;
+            DepthStencilState depthStencil;
+        };
+
         struct Descriptor {
+            PipelineState state;
+            uint32_t pipelineLayout = 0;
+            uint32_t renderPass = 0;
         };
 
         bool Init(const Descriptor&);
--- vulkan/src/GraphicsPipeline.cpp
+++ vulkan/src/GraphicsPipeline.cpp
@@ -14,23 +14,92 @@ namespace sky::drv {
 
     }
 
-    bool GraphicsPipeline::Init(const Descriptor&)
+    bool GraphicsPipeline::Init(const Descriptor& des)
     {
         VkGraphicsPipelineCreateInfo pipelineInfo = {};
         pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+
+        /* Shader */
         pipelineInfo.stageCount;
         pipelineInfo.pStages;
+
         pipelineInfo.pVertexInputState;
-        pipelineInfo.pInputAssemblyState;
-        pipelineInfo.pTessellationState;
-        pipelineInfo.pViewportState;
-        pipelineInfo.pRasterizationState;
-        pipelineInfo.pMultisampleState;
-        pipelineInfo.pDepthStencilState;
-        pipelineInfo.pColorBlendState;
-        pipelineInfo.pDynamicState;
-        pipelineInfo.layout;
-        pipelineInfo.renderPass;
+
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo = {};
+        inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssemblyInfo.topology = des.state.inputAssembly.topology;
+        inputAssemblyInfo.primitiveRestartEnable = des.state.inputAssembly.primitiveRestartEnable;
+        pipelineInfo.pInputAssemblyState = &inputAssemblyInfo;
+
+        pipelineInfo.pTessellationState = nullptr;
+
+        VkPipelineViewportStateCreateInfo vpState = {};
+        vpState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        vpState.viewportCount = 1;
+        vpState.scissorCount = 1;
+        pipelineInfo.pViewportState = &vpState;
+
+        VkPipelineRasterizationStateCreateInfo rasterState = {};
+        rasterState.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterState.depthClampEnable = des.state.raster.depthClampEnable;
+        rasterState.rasterizerDiscardEnable = des.state.raster.rasterizerDiscardEnable;
+        rasterState.polygonMode = des.state.raster.polygonMode;
+        rasterState.cullMode = des.state.raster.cullMode;
+        rasterState.frontFace = des.state.raster.frontFace;
+        rasterState.depthBiasEnable = des.state.raster.depthBiasEnable;
+        rasterState.depthBiasConstantFactor = des.state.raster.depthBiasConstantFactor;
+        rasterState.depthBiasClamp = des.state.raster.depthBiasClamp;
+        rasterState.depthBiasSlopeFactor = des.state.raster.depthBiasSlopeFactor;
+        rasterState.lineWidth = des.state.raster.lineWidth;
+        pipelineInfo.pRasterizationState = &rasterState;
+
+        VkPipelineMultisampleStateCreateInfo multiSampleInfo = {};
+        multiSampleInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multiSampleInfo.rasterizationSamples = des.state.multiSample.rasterizationSamples;
+        multiSampleInfo.sampleShadingEnable = des.state.multiSample.sampleShadingEnable;
+        multiSampleInfo.minSampleShading = des.state.multiSample.minSampleShading;
+        multiSampleInfo.pSampleMask = nullptr;
+        multiSampleInfo.alphaToCoverageEnable = des.state.multiSample.alphaToCoverageEnable;
+        multiSampleInfo.alphaToOneEnable = des.state.multiSample.alphaToOneEnable;
+        pipelineInfo.pMultisampleState = &multiSampleInfo;
+
+        VkPipelineDepthStencilStateCreateInfo dsInfo = {};
+        dsInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+        dsInfo.depthTestEnable = des.state.depthStencil.depthTestEnable;
+        dsInfo.depthWriteEnable = des.state.depthStencil.depthWriteEnable;
+        dsInfo.depthCompareOp = des.state.depthStencil.depthCompareOp;
+        dsInfo.depthBoundsTestEnable = des.state.depthStencil.depthBoundsTestEnable;
+        dsInfo.stencilTestEnable = des.state.depthStencil.stencilTestEnable;
+        dsInfo.front = des.state.depthStencil.front;
+        dsInfo.back = des.state.depthStencil.back;
+        dsInfo.minDepthBounds = des.state.depthStencil.minDepthBounds;
+        dsInfo.maxDepthBounds = des.state.depthStencil.maxDepthBounds;
+        pipelineInfo.pDepthStencilState = &dsInfo;
+
+        VkPipelineColorBlendStateCreateInfo blendInfo = {};
+        blendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        blendInfo.logicOpEnable = VK_FALSE;
+        blendInfo.logicOp = VK_LOGIC_OP_CLEAR;
+        blendInfo.attachmentCount = static_cast<uint32_t>(des.state.blends.attachments.size());
+        blendInfo.pAttachments = des.state.blends.attachments.data();
+        blendInfo.blendConstants[0] = 0;
+        blendInfo.blendConstants[1] = 0;
+        blendInfo.blendConstants[2] = 0;
+        blendInfo.blendConstants[3] = 0;
+        pipelineInfo.pColorBlendState = &blendInfo;
+
+        VkPipelineDynamicStateCreateInfo dynStates = {};
+        dynStates.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+        VkDynamicState dynStatesData[] = {
+            VK_DYNAMIC_STATE_VIEWPORT,
+            VK_DYNAMIC_STATE_SCISSOR
+        };
+        dynStates.dynamicStateCount = sizeof(dynStatesData) / sizeof(VkDynamicState);
+        dynStates.pDynamicStates = dynStatesData;
+        pipelineInfo.pDynamicState = &dynStates;
+
+        pipelineInfo.layout = device.GetPipelineLayout(des.pipelineLayout);
+        pipelineInfo.renderPass = device.GetRenderPass(des.renderPass);
         pipelineInfo.subpass = 0;
         pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
         pipelineInfo.basePipelineIndex = 0;
