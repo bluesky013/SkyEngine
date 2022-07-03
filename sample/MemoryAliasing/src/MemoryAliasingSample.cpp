904f800b7f7e1932fb182b97678299c3dc9449b3
Zach Lee <bluesky013@126.com>
2022-07-04 07:34:02 +0800

    [feature]: FrameGraph.

---
+++ sample/MemoryAliasing/src/MemoryAliasingSample.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Zach Lee on 2022/6/16.
+//
+
+
+#include <MemoryAliasingSample.h>
+#include <vulkan/MemoryPool.h>
+
+namespace sky::render {
+
+    void MemoryAliasingSample::Init()
+    {
+    }
+
+    void MemoryAliasingSample::Start()
+    {
+    }
+
+    void MemoryAliasingSample::Stop()
+    {
+    }
+
+    void MemoryAliasingSample::Tick(float delta)
+    {
+    }
+}
+
+REGISTER_MODULE(sky::render::MemoryAliasingSample)
