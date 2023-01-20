10dd6f76adaba0c782e8c74dadfe35af7c3f03d1
Lizhen <bluesky013@126.com>
2023-01-21 00:57:02 +0800

    [3rd]: added boost_container.

---
+++ cmake/thirdparty/Findboost.cmake
@@ -0,0 +1,21 @@
+set(LIB_NAME "boost")
+
+set(${LIB_NAME}_INCLUDE_DIR ${${LIB_NAME}_PATH})
+set(${LIB_NAME}_LIBS_DIR ${${LIB_NAME}_PATH}/stage/lib)
+
+set(${LIB_NAME}_LIBRARY
+    ${${LIB_NAME}_LIBS_DIR}/Debug/${CMAKE_STATIC_LIBRARY_PREFIX}Imgui${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+set(BOOST_LIB_NAMES
+    container
+    )
+
+foreach(lib ${BOOST_LIB_NAMES})
+    set(TARGET_WITH_NAMESPACE "3rdParty::${LIB_NAME}_${lib}")
+    set(TMP_LIB ${${LIB_NAME}_LIBS_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${LIB_NAME}_${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
+    add_library(${TARGET_WITH_NAMESPACE}  INTERFACE IMPORTED GLOBAL)
+    target_include_directories(${TARGET_WITH_NAMESPACE} INTERFACE ${${LIB_NAME}_INCLUDE_DIR})
+    target_link_libraries(${TARGET_WITH_NAMESPACE} INTERFACE ${TMP_LIB})
+endforeach()
+
+set(${LIB_NAME}_FOUND True)
