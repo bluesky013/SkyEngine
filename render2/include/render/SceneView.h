5154af38c0abfeae9c01cb478f62d2573d314a35
Lizhen <bluesky013@126.com>
2023-06-17 15:44:35 +0800

    [feature]: remove render.

--- render2/include/render/SceneView.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Zach Lee on 2023/4/2.
-//
-
-#pragma once
-
-namespace sky {
-
-} // namespace sky
--- render2/src/RenderScene.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Zach Lee on 2023/4/2.
-//
-
-#include <render/RenderScene.h>
-
-namespace sky {
-
-} // namespace sky
