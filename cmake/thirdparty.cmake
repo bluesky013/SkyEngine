6580f5797f6cf45834ff7be1646085280cdd0b56
Lizhen <bluesky013@126.com>
2026-03-29 02:55:57 +0800

    [3rd]: update find 3rd.

--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -32,28 +32,12 @@ if(EXISTS ${3RD_PATH})
     # imgui
     sky_find_3rd(TARGET imgui         DIR imgui)
 
-    # shader
-    sky_find_3rd(TARGET glslang       DIR glslang)
-    sky_find_3rd(TARGET SPIRVCross    DIR SPIRV-Cross)
-    if (WIN32)
-        sky_find_3rd(TARGET dxcompiler    DIR dxcompiler)
-    endif ()
-
     # test
     sky_find_3rd(TARGET googletest    DIR googletest)
 
     if (SKY_USE_TRACY)
         sky_find_3rd(TARGET tracy         DIR tracy)
         add_definitions(-DTRACY_ENABLE)
-    endif ()
-
-    if (SKY_BUILD_EDITOR)
-        sky_find_3rd(TARGET assimp        DIR assimp)
-        sky_find_3rd(TARGET meshoptimizer DIR meshoptimizer)
-        sky_find_3rd(TARGET stb           DIR stb)
-        sky_find_3rd(TARGET ispc_texcomp  DIR ispc_texcomp)
-        sky_find_3rd(TARGET GKlib         DIR GKlib)
-        sky_find_3rd(TARGET metis         DIR metis)
     endif ()
 else()
     message(FATAL_ERROR "3rdParty folder: ${3RD_PATH} does not exist, call cmake defining a valid 3RD_PATH")
